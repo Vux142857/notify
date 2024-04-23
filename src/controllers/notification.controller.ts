a14e31d7f3d152e63978e04b9a544f081471855d
ttvu <thanhvu6a1@gmail.com>
2024-04-23 22:53:13 +0700

    fix: tsconfig

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express"
 import notificationService from "../services/notification.services"
 import HTTP_STATUS from "../const/httpStatus"
+import { TokenPayload } from "~/models/requests/User.requests"
 
 export const getNotifications = async (req: Request, res: Response) => {
     const { decoded_authorization } = req
     const { skip, limit } = req.query
-    const { user_id } = decoded_authorization
+    const { user_id } = decoded_authorization as TokenPayload
     const notifications = await notificationService.getNotificationsByUserID(
-        user_id, 
-        parseInt(skip as string), 
+        user_id,
+        parseInt(skip as string),
         parseInt(limit as string))
     const status = notifications.length > 0 ? HTTP_STATUS.OK : HTTP_STATUS.NO_CONTENT
     const result = notifications.length > 0 ? notifications : { message: 'No notifications found' }
